b8b2f877a01ca6471599c0221f8982d3c87d8137
shak mohammed <shakeelmohd2012@gmail.com>
2021-01-17 14:06:46 +0000

    extract title into its own component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import data from './data';
 import Article from './Article';
+import Title from './Title';
 
 function App() {
   const [people, setPeople] = useState(data);
@@ -26,11 +27,7 @@ function App() {
 
   return (
     <section className="section">
-      <div className="title">
-        <h2>
-          <span>/</span>reviews
-        </h2>
-      </div>
+      <Title />
       <div className="section-center">
         {people.map((person, personIndex) => {
           let position = 'nextSlide';
